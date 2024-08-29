29bad7bf3d4d168319a34244d3bf6407727afb34
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-08-29 04:28:24 +0000

    debug[vercel]: adding suspense boundary For vercel build error useSearchParams() should be wrapped in a suspense boundary at page "/course/edit". Read more: https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout

--- app/course/edit/page.tsx
+++ app/course/edit/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useEffect, useState } from "react"
+import { useEffect, useState, Suspense } from "react"
 import { useSearchParams } from "next/navigation"
 import CourseEdit from "@/app/course/edit/courseEdit"
 import MissionEdit from "@/app/course/edit/missionEdit"
@@ -9,58 +9,60 @@ import { getCourse } from "@/app/course/listUtils"
 import { finModal, saveModal } from "@/app/components/course/modals"
 
 export default function Edit() {
-	const searchParams = useSearchParams()
-	const [isMissionEdit, setIsMissionEdit] = useState(false)
-	const [modalOpen, setModalOpen] = useState(0)
-	const [field, setField] = useState<FieldState>(initializeField())
-	const [name, setName] = useState<string>("")
+    const searchParams = useSearchParams()
+    const [isMissionEdit, setIsMissionEdit] = useState(false)
+    const [modalOpen, setModalOpen] = useState(0)
+    const [field, setField] = useState<FieldState>(initializeField())
+    const [name, setName] = useState<string>("")
 
-	useEffect(() => {
-		async function fetchCourseData() {
-			const id = Number(searchParams.get("id"))
-			if (id) {
-				const course = await getCourse(id)
-				if (course) {
-					if (course.field) setField(deserializeField(course.field))
-					if (course.name) setName(course.name)
-				}
-			}
-		}
-		fetchCourseData()
-	}, [searchParams])
+    useEffect(() => {
+        async function fetchCourseData() {
+            const id = Number(searchParams.get("id"))
+            if (id) {
+                const course = await getCourse(id)
+                if (course) {
+                    if (course.field) setField(deserializeField(course.field))
+                    if (course.name) setName(course.name)
+                }
+            }
+        }
+        fetchCourseData()
+    }, [searchParams])
 
-	const handleButtonClick = (id: number) => {
-		setModalOpen(id)
-	}
+    const handleButtonClick = (id: number) => {
+        setModalOpen(id)
+    }
 
-	return (
-		<>
-			{isMissionEdit ? (
-				<div className="grid grid-cols-2">
-					<div>
-						<CourseEdit field={field} setField={setField} />
-					</div>
-					<div>
-						<MissionEdit />
-					</div>
-				</div>
-			) : (
-				<CourseEdit field={field} setField={setField} />
-			)}
-			<button className="btn btn-primary min-w-28 max-w-fit mx-auto" onClick={() => handleButtonClick(2)}>
-				コースを保存
-			</button>
-			<button
-				className="btn btn-primary min-w-28 max-w-fit mx-auto"
-				onClick={() => setIsMissionEdit(!isMissionEdit)}>
-				{isMissionEdit ? "ミッション編集を終了" : "ミッション編集"}
-			</button>
-			<button className="btn btn-primary min-w-28 max-w-fit mx-auto" onClick={() => handleButtonClick(1)}>
-				一覧に戻る
-			</button>
+    return (
+        <>
+            <Suspense fallback={<div>Loading...</div>}>
+                {isMissionEdit ? (
+                    <div className="grid grid-cols-2">
+                        <div>
+                            <CourseEdit field={field} setField={setField} />
+                        </div>
+                        <div>
+                            <MissionEdit />
+                        </div>
+                    </div>
+                ) : (
+                    <CourseEdit field={field} setField={setField} />
+                )}
+                <button className="btn btn-primary min-w-28 max-w-fit mx-auto" onClick={() => handleButtonClick(2)}>
+                    コースを保存
+                </button>
+                <button
+                    className="btn btn-primary min-w-28 max-w-fit mx-auto"
+                    onClick={() => setIsMissionEdit(!isMissionEdit)}>
+                    {isMissionEdit ? "ミッション編集を終了" : "ミッション編集"}
+                </button>
+                <button className="btn btn-primary min-w-28 max-w-fit mx-auto" onClick={() => handleButtonClick(1)}>
+                    一覧に戻る
+                </button>
 
-			{modalOpen === 1 && finModal({ setModalOpen })}
-			{modalOpen === 2 && saveModal({ setModalOpen, name, setName, field })}
-		</>
-	)
+                {modalOpen === 1 && finModal({ setModalOpen })}
+                {modalOpen === 2 && saveModal({ setModalOpen, name, setName, field })}
+            </Suspense>
+        </>
+    )
 }
