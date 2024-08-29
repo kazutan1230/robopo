1f715817a7b54b0ab56d4ae171ae81e08cb5a15e
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-08-29 04:01:53 +0000

    refactor[vercel]: vercel build error Type error: Layout "app/layout.tsx" has an invalid "default" export:   Type "Readonly<{ children: ReactNode; modal: ReactNode; }>" is not valid.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,32 +6,29 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "ROBOPO",
-  description: "robopo",
-};
+    title: "ROBOPO",
+    description: "robopo",
+}
 
 export default function RootLayout({
-  children,
-  modal
-}: Readonly<{
-  children: React.ReactNode;
-  modal: React.ReactNode;
+    children,
+}: // modal
+Readonly<{
+    children: React.ReactNode
+    // modal: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="ja"
-      className={inter.className}
-    >
-      <body className="font-zenKakuGothicNew">
-        <Header />
-        {/* <main className="max-w-screen-md mx-auto text-xs sm:px-12 lg:text-base"> */}
-        <main className="grid gap-6 items-center justify-center px-4 py-6 sm:px-6 lg:px-8">
-          {children}
-          {modal}
-          {/* <ScrollToTop /> */}
-        </main>
-        {/* <Footer /> */}
-      </body>
-    </html>
-  )
+    return (
+        <html lang="ja" className={inter.className}>
+            <body className="font-zenKakuGothicNew">
+                <Header />
+                {/* <main className="max-w-screen-md mx-auto text-xs sm:px-12 lg:text-base"> */}
+                <main className="grid gap-6 items-center justify-center px-4 py-6 sm:px-6 lg:px-8">
+                    {children}
+                    {/* {modal} */}
+                    {/* <ScrollToTop /> */}
+                </main>
+                {/* <Footer /> */}
+            </body>
+        </html>
+    )
 }
