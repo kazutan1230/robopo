18bbe86d1763404014bfcb276975ff61d085e036
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2024-09-24 04:55:03 +0000

    refactor: field size change -フィールドを5x5から3x3に変更 -集計結果を都度取得するように変更

--- app/api/summary/[competitionId]/[courseId]/route.ts
+++ app/api/summary/[competitionId]/[courseId]/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server"
 import { getPlayerStats } from "@/app/lib/db/queries/tablequeries" // クエリ関数をインポート
 
+export const revalidate = 0
+
 export async function GET(req: Request, { params }: { params: { competitionId: string; courseId: string } }) {
   const competitionId = parseInt(params.competitionId)
   const courseId = parseInt(params.courseId)
