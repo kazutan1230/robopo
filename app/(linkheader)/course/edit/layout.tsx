4a13cbded6935b967a9bf459c4e6be680fe51ddb
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2025-01-24 01:50:24 +0000

    feat: add layout component with Suspense and PageLoading integration

---
+++ app/(linkheader)/course/edit/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next"
+import { Inter } from "next/font/google"
+import { Suspense } from "react"
+import { Header } from "@/app/components/header"
+import "@/app/globals.css"
+import { PageLoading } from "@/app/components/parts/pageLoading"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata: Metadata = {
+  title: "ROBOPO",
+  description: "robopo",
+}
+
+export default function Layout({
+  children,
+}: // modal
+Readonly<{
+  children: React.ReactNode
+}>) {
+  return <Suspense fallback={<PageLoading />}>{children}</Suspense>
+}
