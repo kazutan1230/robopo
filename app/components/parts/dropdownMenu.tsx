ec0f5882d53c1031dd8f9bd747bff6103b07e928
kazutan1230 <kazuki.nagai@bnt.benextgroup.jp>
2025-02-19 01:59:22 +0000

    fix: update dropdown menu component for improved functionality and styling -TailwindCSS Ver3->4移行に伴い、ハンバーガーメニューが出なくなっていたのを修正。

--- app/components/parts/dropdownMenu.tsx
+++ app/components/parts/dropdownMenu.tsx
@@ -1,72 +1,94 @@
 "use client"
+import { useState } from "react"
 import Link from "next/link"
-import { useEffect, useState, useRef } from "react"
+import { usePathname } from "next/navigation"
 
-const DropdownMenu = (): React.JSX.Element => {
-  const [isOpen, setOpen] = useState(false)
-  const ref = useRef<HTMLDivElement>(null)
+const DropdownMenu = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+  return (
+    <nav className="dropdown dropdown-end md:hidden fixed right-[5%] z-50 space-y-2">
+      <button
+        className="navbar-burger flex items-center p-3 flex-row-reverse flex-grow"
+        onClick={() => setIsMenuOpen(!isMenuOpen)}>
+        <svg className="block h-6 w-6 fill-current" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+          <title>Mobile menu</title>
+          <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+        </svg>
+      </button>
+      <div
+        className={`${
+          isMenuOpen ? "flex" : "hidden"
+        } absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 lg:flex lg:mx-auto lg:items-center lg:w-auto lg:space-x-6`}></div>
+      {isMenuOpen && (
+        <div className="navbar-menu relative z-50">
+          <div
+            className="navbar-backdrop fixed inset-0 bg-neutral-800 opacity-75"
+            onClick={() => setIsMenuOpen(false)}></div>
+          <div className="fixed top-0 left-0 bottom-0 flex flex-col w-4/6 max-w-sm py-6 px-6 bg-neutral-500/90 overflow-y-auto">
+            {/* Logo & Close button */}
+            <div className="flex items-center mb-8">
+              <Link href="/">
+                <div className="mr-auto text-3xl font-bold leading-none">
+                  <div className="textShadow_wt text-white text-[2.2rem] font-bold text-nowrap max-[1000px]:text-[1.4rem] ">
+                    メニュー
+                  </div>
+                </div>
+              </Link>
+              <button className="navbar-close ml-14" onClick={() => setIsMenuOpen(false)}>
+                <svg
+                  className="h-6 w-6 text-neutral-950 cursor-pointer hover:text-gray-700"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path d="M6 18L18 6M6 6l12 12"></path>
+                </svg>
+              </button>
+            </div>
+            <div>
+              <NavLinks setIsMenuOpen={setIsMenuOpen} />
+            </div>
+          </div>
+        </div>
+      )}
+    </nav>
+  )
+}
 
-  // メニュー外をクリックしたら閉じる
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (ref.current && !ref.current.contains(event.target as Node)) {
-        setOpen(false)
-      }
-    }
+type PropsType = {
+  propClass?: string
+  setIsMenuOpen?: (arg0: boolean) => void
+}
 
-    window.addEventListener("click", handleClickOutside)
+const NavLinks = ({ propClass, setIsMenuOpen }: PropsType) => {
+  const currentPath = usePathname()
 
-    return () => {
-      window.removeEventListener("click", handleClickOutside)
-    }
-  }, [])
-
-  // メニューの開閉をトグル
-  const toggleMenu = (e: React.MouseEvent) => {
-    e.stopPropagation() // メニューのクリックをイベント伝播から除外
-    setOpen(!isOpen)
-  }
+  const links = [
+    { label: "HOME", href: "/" },
+    { label: "コース作成", href: "/course" },
+    { label: "選手登録", href: "/player" },
+    { label: "採点者登録", href: "/umpire" },
+  ]
 
   return (
-    <div ref={ref} className="dropdown dropdown-end md:hidden fixed right-[5%] z-50 space-y-2">
-      <div
-        tabIndex={0}
-        role="button"
-        className="btn btn-ghost btn-circle"
-        onClick={toggleMenu} // メニューを開閉
-      >
-        <svg
-          className="w-[48px] h-[48px] text-gray-800 dark:text-white"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          fill="none"
-          viewBox="0 0 24 24">
-          {isOpen ? (
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M6 18 17.94 6M18 18 6.06 6"
-            />
-          ) : (
-            <path stroke="currentColor" strokeLinecap="round" strokeWidth="2" d="M5 7h14M5 12h14M5 17h14" />
-          )}
-        </svg>
-      </div>
-      {isOpen && (
-        <ul
-          tabIndex={0}
-          className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm"
-          onClick={() => setOpen(false)} // メニュー内のアイテムクリック時に閉じる
-        >
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-        </ul>
-      )}
+    <div
+      className={`flex gap-3 mx-3 max-[899px]:flex-col max-[899px]:text-white
+      max-[899px]:gap-9 max-[767px]:w-[fit-content] max-[767px]:last:w-[70%] ${propClass}`}>
+      {links.map((link) => (
+        <Link
+          //? Close hamburger menu
+          onClick={() => setIsMenuOpen && setIsMenuOpen(false)}
+          href={link.href}
+          className={`w-[fit-content] hover:bg-slate-500 hover:text-zinc/60 px-3 py-1 rounded-md text-sm font-medium cursor-pointer text-[1.1rem]
+            ${
+              currentPath == link.href
+                ? "bg-slate-700 cursor-default shadow-1 text-lime-300/70 hover:text-lime-300/80 "
+                : ""
+            }`}
+          key={link.label}>
+          {link.label}
+        </Link>
+      ))}
     </div>
   )
 }
