a4513b899f9f48f7c180382a86d5bbe085b03e59
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-05-23 17:36:36 +0900

    chore: change `next.config.mjs` to `next.config.ts` (#32)

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack(config, options) {
+import type { NextConfig } from "next"
+
+const nextConfig: NextConfig = {
+  webpack(config) {
     config.module.rules.push({
       test: /\.(mp3)$/,
       type: "asset/resource",
