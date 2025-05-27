42f30dad20a5a2f5f694b7a6df70d650dfcd2b8a
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-05-27 16:48:29 +0900

    chore: remove unnecessary `@neondatabase/serverless` (#34)

--- app/lib/db/db.ts
+++ app/lib/db/db.ts
@@ -1,8 +1,6 @@
 import { drizzle } from "drizzle-orm/neon-http"
-import { neon } from "@neondatabase/serverless"
 
 if (process.env.DATABASE_URL === undefined) {
   throw new Error("DATABASE_URL is not defined")
 }
-const sql = neon(process.env.DATABASE_URL)
-export const db = drizzle(sql)
+export const db = drizzle(process.env.DATABASE_URL)
