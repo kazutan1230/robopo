4200860881b4fbb1d9f2d1717f93094626fb5d91
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-06-02 13:08:15 +0900

    docs: enable to hide sidebar (#55)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -66,6 +66,11 @@ const config: Config = {
   themeConfig: {
     // Replace with your project's social card
     // image: "img/docusaurus-social-card.jpg",
+    docs: {
+        sidebar: {
+          hideable: true,
+        },
+      },
     navbar: {
       title: "ROBOPOマニュアル",
       logo: {
