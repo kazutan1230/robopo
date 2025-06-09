12f8051569b11c251d958cd73031ee4dc92d0acb
マルコメ <akio.muto@bnt.benextgroup.jp>
2025-06-09 11:22:50 +0900

    chore(part1): 🔷 add Biome, formatter and linter (#76)

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,11 @@
 export { auth as middleware } from "@/auth"
 
 export const config = {
-    matcher: [
-        "/config/:path*",
-        "/course/:path*",
-        "/player/:path*",
-        "/umpire/:path*",
-        "/summary/:path*",
-    ],
+  matcher: [
+    "/config/:path*",
+    "/course/:path*",
+    "/player/:path*",
+    "/umpire/:path*",
+    "/summary/:path*",
+  ],
 }
